55f4901dabdc5cb5beea7f7404164db6f9e1afc8
Saikat Mondal <saikatmondal15@gmail.com>
2017-06-01 00:02:31 +0530

    rendering html page through the view engine

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
+var handlebars = require('handlebars');
+//var fs = require('fs');
+var path = require('path');
+var engines = require('consolidate');
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+//var viewPath = path.join(__dirname, 'views');
+//app.set('views', viewPath);
+//app.set('view engine', 'html');
+
 app.get('/check/me', function (req, res) {
-  res.send('Hello World!')
+  res.render('checkme.html');
 });
 app.get('/', function (req, res) {
-  res.send('Hi There!')
+  res.render('index.html');
 });
 
 
